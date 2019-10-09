2149f4c87121e9f165031bdafee749762f705f1f
Hassan Shahbazi <h-shahbazi@hotmail.com>
2019-10-09 18:28:48 +0300

    add a sample of fetching access token

--- Sources/VismaSign/main.swift
+++ Sources/VismaSign/main.swift
@@ -1,2 +1,17 @@
 import Foundation
 import VismaSignClient
+
+func fetchAccessToken() {
+    let apiService: VismaSignClientPartner = VismaSignClientImpl()
+    let request = AccessTokenRequest()
+    do {
+        try apiService.fetchAccessToken(clientID: "57b22247-2bce-4dec-9e5f-3b5d895c1687", clientSecret: "XkhbP8UpK7vUy5zLwEU1yfswHTyIt1SR") { result in
+            switch result {
+                case .success(let accessToken):
+                    print("Service call respons: \(accessToken.accessToken)")
+                case .failure(let error):
+                    print("Service call error: \(error.description)")
+            }
+        }
+    }
+}
